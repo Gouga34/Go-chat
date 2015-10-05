dd33696444a3042bd161308c421464253e088642
Chataigner Manuel <manuel.chataigner@etu.umontpellier.fr>
2015-10-05 11:55:48 +0200

    Main.go : - passage aux sockets io - Méthodes de mise en place du serveur et de la gestion des messages (CreateServer, OnConnection, Listen, CreateHandler, CreateAndInitServer)
    Index.html :
    - utilisation websockets io

--- server/main.go
+++ server/main.go
@@ -1,15 +1,67 @@
 package main
 
 import (
+	"github.com/googollee/go-socket.io"
 	"log"
-	"projet/server/server"
+	"net/http"
 )
 
+//CreateServer créé un nouveau serveur
+func CreateServer() *socketio.Server {
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return server
+}
+
+//OnConnection donne les actions à effectuer lorsque l'on a une connexion
+func OnConnection(so socketio.Socket) {
+	log.Println("Client connection")
+
+	so.Join("chat")
+	log.Println("Client join chat room")
+
+	so.On("message", func(msg string) {
+		log.Println(msg)
+		so.Emit("message", msg)
+		so.BroadcastTo("chat", "message", msg)
+	})
+
+	so.On("disconnection", func() {
+		log.Println("on disconnect")
+	})
+}
+
+//Listen écoute le port
+func Listen(port string) {
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+//CreateHandler crée les handlers
+func CreateHandler(server *socketio.Server) {
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+}
+
+//CreateAndInitServer crée le serveur
+func CreateAndInitServer() *socketio.Server {
+	server := CreateServer()
+
+	server.On("connection", OnConnection)
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	return server
+}
+
 func main() {
-	log.Println("Server is running...")
 
-	var myServer server.Server
+	server := CreateAndInitServer()
 
-	myServer.CreateRouter()
-	myServer.Listen(server.PORT)
+	CreateHandler(server)
+	log.Println("Serving at localhost:5000...")
+	Listen(":5000")
 }
